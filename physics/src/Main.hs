f0f9efc34f021cf8143d31991e4bb9dc20f30a11
Dominic Prior <dominicprior@yahoo.co.uk>
2016-11-13 12:04:21 +0000

    Add physics data types

--- physics/src/Main.hs
+++ physics/src/Main.hs
@@ -3,3 +3,33 @@ module Main where
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+data World = World {
+    _blobs :: [Blob]
+  , _worldSize :: Vec  -- in pixels.
+  , _gravity :: Double -- in pixels per second per second.
+}
+
+data Blob = Blob {
+    _pos :: Vec     -- in pixels, from bottom left.
+  , _vel :: Vec     -- in pixels per second.
+  , _rad :: Double  -- in pixels.
+  , _col :: String
+}
+
+data Vec = Vec {
+    vecX :: Double
+  , vecY :: Double
+}
+
+type Time = Double
+type BlobId = Int
+
+data Collision = Collision {
+    _collisionTime :: Time
+  , _who :: WhoCollided
+}
+
+data WhoCollided = BlobBlob BlobId BlobId
+                 | Side BlobId
+                 | CeilingOrFloor BlobId
