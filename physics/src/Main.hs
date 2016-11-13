3f92bb3cf8e77135d073e4b1faaef7863bd2d40a
Dominic Prior <dominicprior@yahoo.co.uk>
2016-11-13 17:25:48 +0000

    Add bounce function etc.

--- physics/src/Main.hs
+++ physics/src/Main.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
+
+import Control.Lens
 
 main :: IO ()
 main = do
@@ -18,18 +22,71 @@ data Blob = Blob {
 }
 
 data Vec = Vec {
-    vecX :: Double
-  , vecY :: Double
+    _vecX :: Double
+  , _vecY :: Double
 }
+
+makeLenses ''World
+makeLenses ''Blob
+makeLenses ''Vec
 
 type Time = Double
 type BlobId = Int
 
 data Collision = Collision {
-    _collisionTime :: Time
-  , _who :: WhoCollided
+    _collisionTime :: Time   -- how far in the future in seconds.
+  , _who :: WhoIsColliding
 }
 
-data WhoCollided = BlobBlob BlobId BlobId
-                 | Side BlobId
-                 | CeilingOrFloor BlobId
+data WhoIsColliding = BlobBlob BlobId BlobId
+                    | Side BlobId
+                    | CeilingOrFloor BlobId
+
+moveForwardsInTime :: Time -> World -> World
+moveForwardsInTime t world =
+  let (Collision dt who) = nextCollision world
+      world' = moveBlobs (min t dt) world
+  in if t < dt then world'
+     else moveForwardsInTime (t - dt) $ bounce who world'
+
+nextCollision :: World -> Collision
+nextCollision = undefined
+
+moveBlobs :: Time -> World -> World
+moveBlobs = undefined
+
+bounce :: WhoIsColliding -> World -> World
+bounce = over blobs . bounce'
+
+bounce' :: WhoIsColliding -> [Blob] -> [Blob]
+bounce' (Side i) bb           = over (element i . vel . vecX) negate bb
+bounce' (CeilingOrFloor i) bb = over (element i . vel . vecY) negate bb
+bounce' (BlobBlob i j) bb =
+  let b = [bb!!i, bb!!j]
+      masses     = map ((^2) . _rad) b
+      velocities = map _vel b
+      momentum = foldl1 (+++) $ zipWith (++*) velocities masses
+      avVel = momentum ++/ sum masses
+      relVels = map (++- avVel) velocities
+      diff = _pos (bb!!j) ++- _pos (bb!!i)
+      [v1,v2] = zipWith f velocities relVels
+      f v rel = v ++- along rel diff
+  in set (element i . vel) v1 $ set (element j . vel) v2 bb
+
+
+(+++) :: Vec -> Vec -> Vec
+(Vec a b) +++ (Vec u v) = Vec (a+u) (b+v)
+(++-) :: Vec -> Vec -> Vec
+(Vec a b) ++- (Vec u v) = Vec (a-u) (b-v)
+(++*) :: Vec -> Double -> Vec
+(Vec u v) ++* k  = Vec (k*u) (k*v)
+(++/) :: Vec -> Double -> Vec
+(Vec u v) ++/ k  = Vec (k/u) (k/v)
+len :: Vec -> Double
+len v = sqrt $ dot v v
+norm :: Vec -> Vec
+norm v = v ++/ len v
+dot :: Vec -> Vec -> Double
+dot (Vec a b) (Vec x y) = a*x + b*y
+along :: Vec -> Vec -> Vec
+along x u = u ++* dot x u ++/ dot u u
