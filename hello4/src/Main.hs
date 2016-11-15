68618ee6c0d7c3a22b59c9cbb1f4b6e2000867d8
Dominic Prior <dominicprior@yahoo.co.uk>
2016-11-15 21:01:23 +0000

    Draw a circle instead of some text

--- hello4/src/Main.hs
+++ hello4/src/Main.hs
@@ -24,7 +24,7 @@ main = run 3708 $ do
     Just body <- getBody doc
     setInnerHTML body $ Just $
         "<h1 id='hi'>Kia ora 5(Hi)</h1>" ++
-        "<svg width=\"200\" height=\"200\">" ++
+        "<svg id='s' width=\"200\" height=\"200\">" ++
         "<circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"green\" stroke-width=\"4\" fill=\"yellow\" />" ++
         "</svg>"
     on doc D.click $ do
@@ -32,21 +32,8 @@ main = run 3708 $ do
         liftIO $ takeMVar state
         liftIO $ putMVar state (x,y)
         moveHi doc x y
-        Just newParagraph <- createElement doc (Just "p")
-        text <- createTextNode doc $ "Click " ++ show (x, y)
-        appendChild newParagraph text
-        appendChild body (Just newParagraph)
         return ()
 
-    -- Make an exit button
-    exitMVar <- liftIO newEmptyMVar
-    Just exit <- createElement doc (Just "span")
-    text <- createTextNode doc "Click here to exit"
-    appendChild exit text
-    appendChild body (Just exit)
-    on exit E.click $ liftIO $ putMVar exitMVar ()
-
-    -- Force all all the lazy evaluation to be executed
     syncPoint
 
     forever $ do
@@ -56,15 +43,7 @@ main = run 3708 $ do
         threadDelay 50000
             
 
-    -- In GHC compiled version the WebSocket connection will end when this
-    -- thread ends.  So we will wait until the user clicks exit.
-    liftIO $ takeMVar exitMVar
-    setInnerHTML body (Just "<h1>Ka kite ano (See you later)</h1>")
-    return ()
-
-
 moveHi doc x y = do
-    Just elt <- getElementById doc "hi"
-    setAttribute elt "style" $
-        "position:relative; color:red; top:" ++
-        show y ++ "px; left:" ++ show x ++ "px;"
+    Just elt <- getElementById doc "s"
+    setInnerHTML elt $ Just $
+        "<circle cx=\"" ++ show x ++ "\" cy=\"" ++ show y ++ "\" r=\"40\" stroke=\"green\" stroke-width=\"4\" fill=\"pink\" />"
